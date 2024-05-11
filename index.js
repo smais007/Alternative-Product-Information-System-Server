93bc52db77e72815210d86766217dfe7828c2806
smais007 <smaisshawon@gmail.com>
2024-05-11 22:58:33 +0600

    inseted data fro, client to server

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const corsConfig = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dmdmzzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const addQueryCollection = client.db("APIS").collection("queries");
+
+    // Adding   Quieries
+    app.post("/queries", async (req, res) => {
+      const addQuery = req.body;
+      const result = await addQueryCollection.insertOne(addQuery);
+      res.send(result);
+    });
+
+    app.get("/queries", async (req, res) => {
+      const cursor = addQueryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/queries", async (req, res) => {
+      const cursor = addQueryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/queries/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addQueryCollection.findOne();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to  APIS");
+});
+
+app.listen(port, () => {
+  console.log("Serever is running");
+});
